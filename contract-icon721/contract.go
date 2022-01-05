77a11a88e81e69b101049d03129c40709ae73b68
Nguyen Dang Nguyen <dangnguyendota@gmail.com>
2022-01-05 18:26:17 +0700

    add icon721, sale2021q4 abi

--- contract-icon721/contract.go
+++ contract-icon721/contract.go
@@ -1,10 +1,11 @@
 package icon721
 
 import (
+	"context"
 	"github.com/adene-develop/adene-goeth/contract"
 	"github.com/adene-develop/adene-goeth/eth"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
 )
 
 const ContractName = "ICON721"
@@ -16,15 +17,14 @@ type ICON721 interface {
 	// InfoWallet returns allocated and remaining allocations number of `user` address
 	// `allocated` is the total number of ICON721 that `user` can be mint
 	// `remainingAllocation` is the remaining number of ICON721 that `user` can be mint
-	InfoWallet(user common.Address) (allocated int64, remainingAllocation int64, err error)
+	InfoWallet(ctx context.Context, user common.Address) (allocated int64, remainingAllocation int64, err error)
 }
 
-func NewIcon721Contract(client *eth.Client, address common.Address, abi abi.ABI) ICON721 {
+func NewIcon721Contract(client *eth.Client, address common.Address) ICON721 {
 	return &icon721{
 		ERC721Enumerable: contract.NewERC721Enumerable(client, address),
 		address:          address,
 		client:           client,
-		abi:              abi,
 	}
 }
 
@@ -33,10 +33,18 @@ type icon721 struct {
 	contract.Ownable
 	address common.Address
 	client  *eth.Client
-	abi     abi.ABI
 }
 
-func (i *icon721) InfoWallet(user common.Address) (allocated int64, remainingAllocation int64, err error) {
-	//TODO implement me
-	panic("implement me")
+func (i *icon721) InfoWallet(ctx context.Context, user common.Address) (allocated int64, remainingAllocation int64, err error) {
+	var result struct {
+		Allocated           uint16
+		RemainingAllocation uint16
+	}
+
+	err = i.client.CallContractViewFunction(ctx, ABI, i.address, &result, "infoWallet", user)
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "icon721 call view `infoWallet` error")
+	}
+
+	return int64(result.Allocated), int64(result.RemainingAllocation), nil
 }
--- contract-sale2021q4/contract.go
+++ contract-sale2021q4/contract.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/adene-develop/adene-goeth/contract"
 	"github.com/adene-develop/adene-goeth/eth"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
 	"math/big"
@@ -13,9 +12,9 @@ import (
 const ContractName = "SALE2021Q4"
 
 type Box struct {
-	Price       int64
-	TotalSupply int64
-	Stock       int64
+	Price       *big.Int
+	TotalSupply *big.Int
+	Stock       *big.Int
 }
 
 type BoxLevel int
@@ -33,20 +32,19 @@ type SALE2021Q4 interface {
 
 	// Info trả về thông tin của contract
 	Info(ctx context.Context) (tokenAddress common.Address, icon721Address common.Address,
-		commonBox Box, rareBox Box, legendaryBox Box)
+		commonBox Box, rareBox Box, legendaryBox Box, err error)
 
 	// InfoWallet trả về thông tin của user
-	InfoWallet(ctx context.Context, user common.Address) (commonBought int, rareBought int, legendaryBought int)
+	InfoWallet(ctx context.Context, user common.Address) (commonBought int, rareBought int, legendaryBought int, err error)
 
 	// BoxLevelOf trả về level của 1 box
 	BoxLevelOf(ctx context.Context, tokenID int64) (level BoxLevel, err error)
 }
 
-func NewSale2021Q4Contract(client *eth.Client, address common.Address, abi abi.ABI) SALE2021Q4 {
+func NewSale2021Q4Contract(client *eth.Client, address common.Address) SALE2021Q4 {
 	return &sale2021q4{
 		address: address,
 		client:  client,
-		abi:     abi,
 	}
 }
 
@@ -55,7 +53,6 @@ type sale2021q4 struct {
 	contract.Ownable
 	address common.Address
 	client  *eth.Client
-	abi     abi.ABI
 }
 
 func (s *sale2021q4) Address() common.Address {
@@ -66,14 +63,35 @@ func (s *sale2021q4) Client() *eth.Client {
 	return s.client
 }
 
-func (s *sale2021q4) Info(ctx context.Context) (tokenAddress common.Address, icon721Address common.Address, commonBox Box, rareBox Box, legendaryBox Box) {
-	//TODO implement me
-	panic("implement me")
+func (s *sale2021q4) Info(ctx context.Context) (tokenAddress common.Address, icon721Address common.Address, commonBox Box, rareBox Box, legendaryBox Box, err error) {
+	var result struct {
+		Token     common.Address
+		Icon721   common.Address
+		Common    Box
+		Rare      Box
+		Legendary Box
+	}
+	err = s.client.CallContractViewFunction(ctx, ABI, s.address, &result, "info")
+	if err != nil {
+		return [20]byte{}, [20]byte{}, Box{}, Box{}, Box{}, errors.Wrap(err, "sale2021q4 call view `info` error")
+	}
+
+	return result.Token, result.Icon721, result.Common, result.Rare, result.Legendary, nil
 }
 
-func (s *sale2021q4) InfoWallet(ctx context.Context, user common.Address) (commonBought int, rareBought int, legendaryBought int) {
-	//TODO implement me
-	panic("implement me")
+func (s *sale2021q4) InfoWallet(ctx context.Context, user common.Address) (commonBought int, rareBought int, legendaryBought int, err error) {
+	var result struct {
+		Common    uint16
+		Rare      uint16
+		Legendary uint16
+	}
+
+	err = s.client.CallContractViewFunction(ctx, ABI, s.address, &result, "infoWallet", user)
+	if err != nil {
+		return 0, 0, 0, errors.Wrap(err, "sale2021q4 call view `infoWallet` error")
+	}
+
+	return int(result.Common), int(result.Rare), int(result.Legendary), nil
 }
 
 func (s *sale2021q4) BoxLevelOf(ctx context.Context, tokenID int64) (BoxLevel, error) {
@@ -81,7 +99,7 @@ func (s *sale2021q4) BoxLevelOf(ctx context.Context, tokenID int64) (BoxLevel, e
 		BoxLevel *big.Int
 	}
 
-	err := s.client.CallContractViewFunction(ctx, s.abi, s.address, &result, "boxLevelOf", big.NewInt(tokenID))
+	err := s.client.CallContractViewFunction(ctx, ABI, s.address, &result, "boxLevelOf", big.NewInt(tokenID))
 	if err != nil {
 		return 0, errors.Wrap(err, "sale2021q4 BoxLevelOf call view error")
 	}
